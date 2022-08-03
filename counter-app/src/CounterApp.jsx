e8b6e93dd4254c5d51b301aaae18306457f55bd2
JosirmaT <81194487+Joshipuedo@users.noreply.github.com>
2022-08-03 16:18:19 -0500

    done

--- counter-app/src/CounterApp.jsx
+++ counter-app/src/CounterApp.jsx
@@ -6,22 +6,29 @@ export const CounterApp = ( { value } ) => {
   
   const [ counter, setCounter ] = useState( value );
   
+  console.log ('render');
+  
   const handleAdd = () => {
     // console.log ('+1')
     setCounter( counter + 1 );
     // setCounter ( (c) => c + 1 );
-
   }
 
+  const handleSubtract = () => {
+    setCounter ( counter - 1);
+  }
+
+  const handleReset = () => {
+    setCounter ( value );
+  }
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { counter } </h2>
-
-      <button onClick= { handleAdd }>
-         +1
-      </button>
+      <button onClick= { handleAdd }> +1 </button>
+      <button onClick= { handleSubtract }> -1 </button>
+      <button onClick= { handleReset }> Reset </button>
     </>
   )
 }
