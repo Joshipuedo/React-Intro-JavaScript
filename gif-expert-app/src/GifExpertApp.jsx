54022c23709f68b9b4c5c04e771d25b4c777ae30
JosirmaT <81194487+Joshipuedo@users.noreply.github.com>
2022-08-04 11:20:47 -0500

    edits

---
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        {/* Título */}
+        <h1>GifExpertApp</h1>
+        
+        {/* Input */}
+
+        {/* Listado de GIFs */}
+            {/* GIF Item */}
+    </>
+  )
+}
