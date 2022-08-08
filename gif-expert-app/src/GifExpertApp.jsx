61eb80956e7e13ea8a85b915f34db2295846df0e
JosirmaT <81194487+Joshipuedo@users.noreply.github.com>
2022-08-08 11:51:10 -0500

    validations

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -6,8 +6,12 @@ export const GifExpertApp = () => {
 
   const [ categories, setCategories ] = useState([ 'One', 'Two' ])
 
-  const onAddCategory = () => {
-    setCategories( [ 'NEW', ...categories ] )
+  const onAddCategory = (newCategory) => {
+
+    if ( categories.includes(newCategory) ) return; 
+      
+
+    setCategories( [ newCategory, ...categories ] )
     //setCategories( cat => [...cat, 'Valorant'])
   }
 
@@ -17,8 +21,12 @@ export const GifExpertApp = () => {
         <h1>GifExpertApp</h1>
         
         {/* Input */}
-        <AddCategory setCategories={ setCategories } />
-
+        <AddCategory 
+         // setCategories={ setCategories } 
+         onNewCategory = { onAddCategory }
+         currentCategories = { categories } 
+         
+         />
         {/* Listado de GIFs */}
         
         <ol>
